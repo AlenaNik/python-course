f244e4b5869bf1e8f4f76955c769149a3b84f838
AlenaNik <alena.nik.digital@gmail.com>
2019-11-16 23:07:57 +0100

    opens up a studio file

--- app.py
+++ app.py
@@ -1,8 +1,43 @@
-weight = int(input('How much is your weight? '))
-unit = input('(L)bs or (K)bg: ')
-if unit.upper() == "L":
-    converted = weight * 0.45
-    print(f"You are {converted} kilos")
-else:
-    converted = weight / 0.45
-    print(f"You are {converted} pounds")
+import tkinter as tk
+from tkinter import filedialog, Text
+import os
+import PyPDF2
+
+root = tk.Tk()
+apps = []
+
+def addApp():
+
+    filename = filedialog.askopenfile(initialdir="/", title="Select File",
+                                      filetypes=(("executables", "*.studio"), ("all files", "*.*")))
+    apps.append(filename)
+    print(filename)
+    for app in apps:
+        label = tk.Label(frame, text=app, bg="white")
+        label.pack()
+
+def readApps():
+    for app in apps:
+        os.startFile(app)
+
+canvas = tk.Canvas(root, height=400, width=400, bg="#0c1013")
+canvas.pack()
+frame = tk.Frame(root, bg="#181f2d")
+frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
+
+openFile = tk.Button(root, text="Open File",
+                     padx=10,
+                     pady=5,
+                     fg="red",
+                     bg="red", command=addApp)
+openFile.pack()
+readApps = tk.Button(root, text="Run",
+                     padx=10,
+                     pady=5,
+                     fg="#ff7d6d",
+                     bg="#0c1013",
+                     command=readApps
+                     )
+readApps.pack()
+
+root.mainloop()
