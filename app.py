c12417f1c5590cf08e01cc049740f3e0f3e58f4f
AlenaNik <alena.nik.digital@gmail.com>
2019-11-16 20:51:47 +0100

    basic if else statements

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
-first = 'Jaime'
-last = 'Smith'
-msg = f'{first} [{last}] is a coder'
-print(msg)
+total_price = 1000000
+lower_price = total_price * 0.2
+higher_price = total_price * 0.1
+
+buyer_has_good_credit = True
+buyer_has_bad_credit = True
+
+if buyer_has_good_credit:
+    print(lower_price)
+elif buyer_has_bad_credit:
+    print(higher_price)
+else:
+    print('What am I doing with my life')
